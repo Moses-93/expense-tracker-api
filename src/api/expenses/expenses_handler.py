9e5a6ee65c805dd38a604ce0bd384a616e10c2ef
Moses-93 <moieseenko2001@gmail.com>
2025-04-04 17:36:55 +0300

    feat: Implement ExpenseHandler and ExpenseRouter for expense management API

---
+++ src/api/expenses/expenses_handler.py
@@ -0,0 +1,81 @@
+import logging
+from fastapi import Query, Depends, Response
+
+from src.services.user_service import get_current_user
+from src.db.models import User
+from src.core.database import get_db
+from src.services.expense.expense_manager import ExpenseManager
+from .schemas import ExpenseCreate, ExpenseUpdate
+
+
+logger = logging.getLogger(__name__)
+
+class ExpenseHandler:
+    def __init__(self, expenses_manager: ExpenseManager):
+        self.expenses_manager = expenses_manager
+    
+    def get_expenses(
+            self,
+            user: User = Depends(get_current_user),
+            session=Depends(get_db)
+            ):
+        return self.expenses_manager.get_expenses(
+            user_id=user.id,
+            session=session
+        )
+    
+    def get_expenses_excel_report(
+            self,
+            user: User = Depends(get_current_user),
+            start_date: str = Query(..., description="Start date for the report"),
+            end_date: str = Query(..., description="End date for the report"),
+            session=Depends(get_db)
+            ):
+        
+        expenses_report = self.expenses_manager.get_expenses_report(
+            user_id=user.id,
+            start_date=start_date,
+            end_date=end_date,
+            format_report='excel',
+            session=session
+        )
+        headers = {
+            "Content-Disposition": "attachment; filename=expenses_report.xlsx"
+        }
+        
+        return Response(content=expenses_report, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers=headers)
+    
+    def create_expense(
+            self,
+            expense: ExpenseCreate,
+            user: User = Depends(get_current_user),
+            session=Depends(get_db)
+            ):
+        created_expensive = self.expenses_manager.create_expense(
+            user_id=user.id,
+            expense=expense,
+            session=session
+        )
+        
+    
+    def update_expense(
+            self,
+            expense_id: int,
+            expense: ExpenseUpdate,
+            session=Depends(get_db)
+            ):
+        self.expenses_manager.update_expense(
+            expense_id=expense_id,
+            expense_update=expense,
+            session=session
+        )
+    
+    def delete_expense(
+            self,
+            expense_id: int,
+            session=Depends(get_db)
+            ):
+        self.expenses_manager.delete_expense(
+            expense_id=expense_id,
+            session=session
+        )
---
+++ src/api/expenses/expenses_router.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, status
+from .expenses_handler import ExpenseHandler
+from .schemas import ExpenseResponse
+
+
+class ExpenseRouter:
+    def __init__(self, expenses_handlers: ExpenseHandler):
+        self.router = APIRouter(prefix="/api/expenses", tags=["expenses"])
+        self.register_handlers(expenses_handlers)
+    
+    def register_handlers(self, expenses_handlers: ExpenseHandler):
+        self.router.add_api_route(
+            path="/",
+            endpoint=expenses_handlers.get_expenses,
+            response_model=list[ExpenseResponse],
+            status_code=status.HTTP_200_OK,
+            methods=["GET"],
+            name="Get Expenses",
+        )
+        self.router.add_api_route(
+            path="/report",
+            endpoint=expenses_handlers.get_expenses_excel_report,
+            methods=["GET"],
+            name="Get Expenses Report",
+        )
+        self.router.add_api_route(
+            path="/",
+            endpoint=expenses_handlers.create_expense,
+            status_code=status.HTTP_201_CREATED,
+            methods=["POST"],
+            name="Create Expense",
+        )
+        self.router.add_api_route(
+            path="/{expense_id}",
+            endpoint=expenses_handlers.update_expense,
+            status_code=status.HTTP_200_OK,
+            methods=["PATH"],
+            name="Update Expense",
+        )
+        self.router.add_api_route(
+            path="/{expense_id}",
+            endpoint=expenses_handlers.delete_expense,
+            status_code=status.HTTP_204_NO_CONTENT,
+            methods=["DELETE"],
+            name="Delete Expense",
+        )
