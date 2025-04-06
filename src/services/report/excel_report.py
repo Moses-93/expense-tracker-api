9571ffba9e472d8d1c24aad285edfde2384fc353
Moses-93 <moieseenko2001@gmail.com>
2025-04-06 09:39:52 +0300

    refactor: Split the ExcelReport class into several methods.
    -  _write_header sets the header and its background.
    - _write_expenses adds values to the fields.
    - _write_totals calculates the total expenses and adds them to the report.
    - _auto_adjust_column_widths sets the column widths based on the content.

--- src/services/report/excel_report.py
+++ src/services/report/excel_report.py
@@ -1,3 +1,4 @@
+import logging
 from io import BytesIO
 from typing import List
 from openpyxl import Workbook
@@ -6,32 +7,47 @@ from src.db.models import Expense
 from .interfaces import ReportBase
 
 
+logger = logging.getLogger(__name__)
+
+
 class ExcelReport(ReportBase):
 
     def generate_report(self, expenses: List[Expense]) -> BytesIO:
-        """
-        Generate an Excel report from a list of expenses.
-        """
         wb = Workbook()
         ws = wb.active
-        ws.title = "Витрати"
+        ws.title = "Expenses"
 
+        self._write_header(ws)
+        self._write_expenses(ws, expenses)
+        self._write_totals(ws, expenses)
+        self._auto_adjust_column_widths(ws)
+
+        output = BytesIO()
+        wb.save(output)
+        output.seek(0)
+        return output
+
+    def _write_header(self, ws):
         headers = ["ID", "Назва", "Дата", "Сума(UAH)", "Сума(USD)"]
         ws.append(headers)
-
         header_font = Font(bold=True)
-        for cell in ws[1]:  
+        for cell in ws[1]:
             cell.font = header_font
 
+    def _write_expenses(self, ws, expenses: List[Expense]):
         for exp in expenses:
+            logger.info(f"Expense: {exp}")
             ws.append([exp.id, exp.name, exp.date, exp.uah_amount, exp.usd_amount])
 
+    def _write_totals(self, ws, expenses: List[Expense]):
         total_sum_uah = sum(exp.uah_amount for exp in expenses)
         total_sum_usd = sum(exp.usd_amount for exp in expenses)
 
-        ws.append(["Загальна сума у гривнях", total_sum_uah])
-        ws.append(["Загальна сума у доларах", total_sum_usd])
+        ws.append([])
+        ws.append(["Сума UAH", total_sum_uah])
+        ws.append(["Сума USD", total_sum_usd])
 
+    def _auto_adjust_column_widths(self, ws):
         for col in ws.columns:
             max_length = 0
             col_letter = col[0].column_letter
@@ -39,8 +55,3 @@ class ExcelReport(ReportBase):
                 if cell.value:
                     max_length = max(max_length, len(str(cell.value)))
             ws.column_dimensions[col_letter].width = max_length + 2
-
-        output = BytesIO()
-        wb.save(output)
-        output.seek(0)
-        return output
