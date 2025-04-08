31025eccda8d6a9e49883d09a3dc205afc412058
Moses-93 <moieseenko2001@gmail.com>
2025-04-08 17:35:13 +0300

    refactor: Remove logging of individual expenses in ExcelReport

--- src/services/report/excel_report.py
+++ src/services/report/excel_report.py
@@ -55,7 +55,6 @@ class ExcelReport(ReportBase):
         Writes the list of expenses into the worksheet.
         """
         for exp in expenses:
-            logger.info(f"Expense: {exp}")
             ws.append([exp.id, exp.name, exp.date, exp.uah_amount, exp.usd_amount])
 
     def _write_totals(self, ws, expenses: List[Expense]):
