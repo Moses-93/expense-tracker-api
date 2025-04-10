14f4cba7a3d665f1e5515db37c4448ea44148eeb
Moses-93 <moieseenko2001@gmail.com>
2025-04-10 10:30:14 +0300

    refactor: Simplify get_report_generator method and improve docstring for clarity

--- src/services/report/report_factory.py
+++ src/services/report/report_factory.py
@@ -3,18 +3,23 @@ from src.services.report.excel_report import ExcelReport
 
 
 class ReportFactory:
-    _instances = {}
+    """
+    A factory class for creating report generator instances based on the specified format.
+    """
 
-    @classmethod
-    def get_report_generator(cls, format_report: str) -> ReportBase:
-        """
-        Returns a report generator instance based on the format.
-        Uses caching to avoid unnecessary instantiations.
-        """
-        if format_report not in cls._instances:
-            if format_report == "excel":
-                cls._instances[format_report] = ExcelReport()
-            else:
-                raise ValueError("Unsupported report format")
+    def get_report_generator(self, format_report: str) -> ReportBase:
+        """Creates and returns a report generator instance based on the provided format.
 
-        return cls._instances[format_report]
+        Args:
+            format_report (str): format_report (str): The format of the report (e.g., "excel").
+
+        Raises:
+            ValueError: ValueError: If the provided format is not supported.
+
+        Returns:
+            ReportBase: ReportBase: An instance of a report generator.
+        """
+        if format_report == "excel":
+            return ExcelReport()
+        else:
+            raise ValueError(f"Unsupported report format: {format_report}")
