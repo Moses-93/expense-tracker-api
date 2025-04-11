39966d66c6f7437fb61f8df961c945d02f7f21c9
Moses-93 <moieseenko2001@gmail.com>
2025-04-11 16:43:43 +0300

    feat: Add a DTO class “GetExpenseParams” to group parameters for getting expenses

---
+++ src/schemas/expense/dto.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+
+@dataclass
+class GetExpenseParams:
+    user_id: int
+    start_date: Optional[date]
+    end_date: Optional[date]
+    all_expenses: Optional[bool]
+    format: str = "excel"
