736656c59fc40ac04457f9a75c55f0e8aa1bbc46
Moses-93 <moieseenko2001@gmail.com>
2025-04-11 17:11:49 +0300

    feat: Implement expense container with service registrations

---
+++ src/api/expenses/expense_container.py
@@ -0,0 +1,15 @@
+import punq
+from src.services.exchange_rate_service import ExchangeRateService
+from src.services.expense import expense_controller, expense_service, expense_repository
+from src.services.report.report_factory import ReportFactory
+from . import expenses_handler, expenses_router
+
+expense_container = punq.Container()
+
+expense_container.register(expense_repository.ExpenseRepository)
+expense_container.register(ExchangeRateService)
+expense_container.register(expense_service.ExpenseService)
+expense_container.register(ReportFactory)
+expense_container.register(expense_controller.ExpenseController)
+expense_container.register(expenses_handler.ExpenseHandler)
+expense_container.register(expenses_router.ExpenseRouter)
